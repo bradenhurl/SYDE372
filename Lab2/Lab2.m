09b3784d8df8b4e76658f7da17791294c5d9d628
Braden Hurl <bradenhurl@users.noreply.github.com>
2015-04-13 21:57:20 -0400

    Create Lab2.m

---
+++ Lab2/Lab2.m
@@ -0,0 +1,13 @@
+% SYDE 372 - Lab 2
+% Braden Hurl - 20406936
+% Nicholas Chen - 20375332
+
+clc;
+close all;
+clear all; 
+
+% Separated for easier debugging
+% Calls part 1
+Part1
+% Call part 2
+Part2
